9cdb382685f2d0900c7917c759db45d056fbf8cb
AlecGrey <alecgrey.coding@gmail.com>
2020-11-01 16:56:15 -0800

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,12 @@
 class Artist < ActiveRecord::Base
+
+    has_many :songs
+
+    def song_count
+        song_count = self.songs.size
+    end
+    
+    def verbose_song_count
+        self.song_count == 1 ? "#{self.song_count} song" : "#{self.song_count} songs"
+    end
 end
